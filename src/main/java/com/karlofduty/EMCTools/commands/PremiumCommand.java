bce79f4970b0d5a58c3444c076d229f491f5fd4d
Karl Essinger <xkaess22@gmail.com>
2020-08-15 12:25:25 +0200

    Updated /premium

--- src/main/java/com/karlofduty/EMCTools/commands/PremiumCommand.java
+++ src/main/java/com/karlofduty/EMCTools/commands/PremiumCommand.java
@@ -1,9 +1,17 @@
 package com.karlofduty.EMCTools.commands;
 
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import java.util.UUID;
+
+import static net.md_5.bungee.api.ChatColor.*;
 
 public class PremiumCommand implements CommandExecutor
 {
@@ -18,11 +26,23 @@ public class PremiumCommand implements CommandExecutor
 
 		if(sender.hasPermission("group.premium"))
 		{
-			sender.sendMessage("§DWow, thanks for supporting the server with your Premium subscription! You are amazing!");
+			sender.sendMessage(new TextComponent(
+				new ComponentBuilder("Wow, thanks for supporting the server with your Premium subscription! You are amazing!")
+					.color(LIGHT_PURPLE)
+					.event(new ClickEvent(ClickEvent.Action.OPEN_URL,"https://store.earthmc.net/premiumcommand"))
+					.event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, TextComponent.fromLegacyText(DARK_PURPLE + "Open EarthMC.net Premium information.")))
+					.create()
+			));
 		}
 		else
 		{
-			sender.sendMessage("§DIf you want to support the server you can buy Premium by clicking here: §3https://store.earthmc.net/premium§D. Premium gives you cosmetic perks and queue priority among other things.");
+			sender.sendMessage(new TextComponent(
+				new ComponentBuilder("Click here to find out more about EarthMC premium!")
+					.color(LIGHT_PURPLE)
+					.event(new ClickEvent(ClickEvent.Action.OPEN_URL,"https://store.earthmc.net/premiumcommand"))
+					.event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, TextComponent.fromLegacyText(DARK_PURPLE + "Open EarthMC.net Premium information.")))
+					.create()
+			));
 		}
 		return true;
 	}
