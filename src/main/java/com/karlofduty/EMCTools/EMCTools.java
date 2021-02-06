c4f23c9abf6724801b49640d2dcd199d686c7896
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2021-02-06 18:12:06 +0100

    Add beta command confirmation.

--- src/main/java/com/karlofduty/EMCTools/EMCTools.java
+++ src/main/java/com/karlofduty/EMCTools/EMCTools.java
@@ -1,15 +1,27 @@
 package com.karlofduty.EMCTools;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.Map.Entry;
+
+import com.karlofduty.EMCTools.commands.BetaCommand;
 import com.karlofduty.EMCTools.commands.JoinQueueCommand;
 import com.karlofduty.EMCTools.commands.PremiumCommand;
+
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.TextComponent;
+
 public class EMCTools extends JavaPlugin
 {
     public static EMCTools instance;
+    public static Map<UUID, Long> awaitingBetaConfirmation = new HashMap<>();
 
     @Override
     public void onEnable()
@@ -18,28 +30,28 @@ public class EMCTools extends JavaPlugin
         getServer().getMessenger().registerOutgoingPluginChannel(this, "queue:join");
         this.getCommand("joinqueue").setExecutor(new JoinQueueCommand());
         this.getCommand("beta").setExecutor(new BetaCommand());
-        this.getCommand("art").setExecutor(new ArtCommand());
         this.getCommand("premium").setExecutor(new PremiumCommand());
+        this.getCommand("towny").setExecutor(new TownyCommand());
+
+        Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, () ->
+        {
+            for (Entry <UUID,Long> currentPlayer : awaitingBetaConfirmation.entrySet()) {
+                if (System.currentTimeMillis() > currentPlayer.getValue()) //Confirmation is expired
+                {
+                    awaitingBetaConfirmation.remove(currentPlayer.getKey());
+                    Bukkit.getPlayer(currentPlayer.getKey()).spigot().sendMessage(new TextComponent(ChatColor.RED + "Beta command confirmation timed out."));
+                }
+            }
+        }, 100, 100);
     }
 
-    private class BetaCommand implements CommandExecutor
+    private class TownyCommand implements CommandExecutor
     {
-        @Override
         public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
         {
-            return getServer().dispatchCommand(sender, "joinqueue beta");
+            return getServer().dispatchCommand(sender, "joinqueue towny");
         }
     }
-
-    private class ArtCommand implements CommandExecutor
-    {
-        @Override
-        public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
-        {
-            return getServer().dispatchCommand(sender, "joinqueue art");
-        }
-    }
-
     public static boolean executeCommand(String command)
     {
         return instance.getServer().dispatchCommand(instance.getServer().getConsoleSender(), command);
