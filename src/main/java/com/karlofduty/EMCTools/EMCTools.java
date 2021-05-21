d1bfcd2b25ae33920c44a19e6cf5a7831311d113
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2021-05-21 23:29:55 +0200

    Add /map command, remove legacy text

--- src/main/java/com/karlofduty/EMCTools/EMCTools.java
+++ src/main/java/com/karlofduty/EMCTools/EMCTools.java
@@ -7,47 +7,36 @@ import java.util.Map.Entry;
 
 import com.karlofduty.EMCTools.commands.BetaCommand;
 import com.karlofduty.EMCTools.commands.JoinQueueCommand;
+import com.karlofduty.EMCTools.commands.MapCommand;
 import com.karlofduty.EMCTools.commands.PremiumCommand;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.chat.TextComponent;
-
-public class EMCTools extends JavaPlugin
-{
+public class EMCTools extends JavaPlugin {
     public static EMCTools instance;
     public static Map<UUID, Long> awaitingBetaConfirmation = new HashMap<>();
 
     @Override
-    public void onEnable()
-    {
+    public void onEnable() {
         instance = this;
         getServer().getMessenger().registerOutgoingPluginChannel(this, "queue:join");
-        this.getCommand("joinqueue").setExecutor(new JoinQueueCommand());
-        this.getCommand("beta").setExecutor(new BetaCommand());
-        this.getCommand("premium").setExecutor(new PremiumCommand());
+        getCommand("joinqueue").setExecutor(new JoinQueueCommand());
+        getCommand("beta").setExecutor(new BetaCommand());
+        getCommand("premium").setExecutor(new PremiumCommand());
+        getCommand("map").setExecutor(new MapCommand());
 
         Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, () ->
         {
-            for (Entry <UUID,Long> currentPlayer : awaitingBetaConfirmation.entrySet()) {
+            for (Entry<UUID, Long> currentPlayer : awaitingBetaConfirmation.entrySet()) {
                 if (System.currentTimeMillis() > currentPlayer.getValue()) //Confirmation is expired
                 {
                     awaitingBetaConfirmation.remove(currentPlayer.getKey());
-                    Bukkit.getPlayer(currentPlayer.getKey()).spigot().sendMessage(new TextComponent(ChatColor.RED + "Beta command confirmation timed out."));
+                    Bukkit.getPlayer(currentPlayer.getKey()).sendMessage(Component.text("Beta command confirmation timed out.").color(NamedTextColor.RED));
                 }
             }
         }, 100, 100);
-    }
-
-    public static boolean executeCommand(String command)
-    {
-        return instance.getServer().dispatchCommand(instance.getServer().getConsoleSender(), command);
-    }
-
-    public static void log(String message)
-    {
-        instance.getServer().getLogger().info(message);
     }
 }
