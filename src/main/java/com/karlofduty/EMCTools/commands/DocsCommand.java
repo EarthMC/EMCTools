b5eb4e3b40e88995ba740fe061fd34f0c060ed9c
Basically_Andrew <59465284+Basically-Andrew@users.noreply.github.com>
2021-12-02 17:03:33 -0600

    Update DocsCommand.java

--- src/main/java/com/karlofduty/EMCTools/commands/DocsCommand.java
+++ src/main/java/com/karlofduty/EMCTools/commands/DocsCommand.java
@@ -11,7 +11,7 @@ public class DocsCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        sender.sendMessage(Component.text("Click here for a link to the online docs.").clickEvent(ClickEvent.openUrl("https://earthmc.net/docs/")));
+        sender.sendMessage(Component.text("Click here for a link to the online docs.").color(NamedTextColor.GREEN).clickEvent(ClickEvent.openUrl("https://earthmc.net/docs/")).hoverEvent(HoverEvent.showText(Component.text("Click me to open!").color(NamedTextColor.GREEN))));
         return true;
     }
 }
