84e932f9b4be186f2fb82efff6ef44c2ea093587
Basically_Andrew <59465284+Basically-Andrew@users.noreply.github.com>
2021-12-02 17:11:07 -0600

    Update DocsCommand.java

--- src/main/java/com/karlofduty/EMCTools/commands/DocsCommand.java
+++ src/main/java/com/karlofduty/EMCTools/commands/DocsCommand.java
@@ -2,6 +2,8 @@ package com.karlofduty.EMCTools.commands;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.event.HoverEvent;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
