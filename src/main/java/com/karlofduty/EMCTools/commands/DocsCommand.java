309253476a08129ff9e15849d2aed051389d63be
Basically_Andrew <59465284+Basically-Andrew@users.noreply.github.com>
2022-03-10 03:54:37 -0600

    Add color & hover text to /docs command (#8)
    * Update DocsCommand.java
    
    * Update DocsCommand.java

--- src/main/java/com/karlofduty/EMCTools/commands/DocsCommand.java
+++ src/main/java/com/karlofduty/EMCTools/commands/DocsCommand.java
@@ -2,6 +2,8 @@ package com.karlofduty.EMCTools.commands;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.event.HoverEvent;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,7 +13,7 @@ public class DocsCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        sender.sendMessage(Component.text("Click here for a link to the online docs.").clickEvent(ClickEvent.openUrl("https://earthmc.net/docs/")));
+        sender.sendMessage(Component.text("Click here for a link to the online docs.").color(NamedTextColor.GREEN).clickEvent(ClickEvent.openUrl("https://earthmc.net/docs/")).hoverEvent(HoverEvent.showText(Component.text("Click me to open!").color(NamedTextColor.GREEN))));
         return true;
     }
 }
