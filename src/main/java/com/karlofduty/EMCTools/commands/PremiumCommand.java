bc2c1a7b98134f5e635ffed6446659f3d28ab218
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2022-07-23 00:29:10 +0200

    Updated premium url

--- src/main/java/com/karlofduty/EMCTools/commands/PremiumCommand.java
+++ src/main/java/com/karlofduty/EMCTools/commands/PremiumCommand.java
@@ -8,6 +8,7 @@ import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class PremiumCommand implements CommandExecutor
 {
@@ -17,10 +18,12 @@ public class PremiumCommand implements CommandExecutor
 		String message = "Click here to find out more about EarthMC premium!";
 		if (sender.hasPermission("group.premium"))
 			message = "Wow, thanks for supporting the server with your Premium subscription! You are amazing!";
+
+		String ignPart = sender instanceof Player player ? "?ign=" + player.getName() : "";
 		
 		TextComponent premiumComponent = Component.text(message, NamedTextColor.LIGHT_PURPLE)
 			.hoverEvent(HoverEvent.showText(Component.text("Open EarthMC.net Premium information.", NamedTextColor.DARK_PURPLE)))
-			.clickEvent(ClickEvent.openUrl("https://store.earthmc.net/premiumcommand"));
+			.clickEvent(ClickEvent.openUrl("https://store.earthmc.net/category/premium" + ignPart));
 
 		sender.sendMessage(premiumComponent);
 		return true;
